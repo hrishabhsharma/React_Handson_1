401fa74576565288bafcc42750b9233f2f8c38ef
hrishabhsharma <sharma17h@gmail.com>
2023-07-12 22:54:15 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
+import React, { useState} from "react"
 import Fcomp from "./Handson_1/FComp.js"
 import Ccomp from "./Handson_1/CComp.js"
 import "./App.css"
 
 
-function btn1(){
-document.querySelector('.box1').style.visibility =  "visible";
-}
-function btn2(){
-document.querySelector('.box2').style.visibility =  "visible";
-}
 
 function App(){
+  const [fState , setFState] = useState(false)
+  const [cState , setCState] = useState(false)
+
   return(
     <div>
     <h1 className="head">Styling using Functional and Class Component</h1>
     <div >
-      <button onClick={btn1} className="btn">To see styling in Functional component</button>
-      <button onClick={btn2} className="btn">To see styling in Class component</button>
+      <button onClick={() => setFState(!fState)} className="btn">To see styling in Functional component</button>
+      <button onClick={() => setCState(!cState)} className="btn">To see styling in Class component</button>
     </div>
     <div className="container">
-    <Fcomp/>
-    <Ccomp/>
+    {fState && <Fcomp/>}
+    {cState && <Ccomp/>}
     </div>
     </div>
   )
